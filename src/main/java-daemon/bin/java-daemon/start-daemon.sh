6483da8f74ddaa42a77977858565aca264c96214
Alessandro Gnoli <tebemis@gmail.com>
2012-04-04 14:43:33 -0500

    Modifier java-daemon script for deploy

--- src/main/java-daemon/bin/java-daemon/start-daemon.sh
+++ src/main/java-daemon/bin/java-daemon/start-daemon.sh
@@ -6,7 +6,6 @@ BIN_DIR="$( dirname "$DAEMON_DIR" )"
 APP_DIR="$( dirname "$BIN_DIR" )"
 # get paths
 PID_FILE="$APP_DIR"/.pid
-OUT_FILE="$APP_DIR"/log/wallcology-notifier.log
 # get java
 JAVA="$( "$DAEMON_DIR"/find-java.sh )"
 # check pid file
@@ -15,10 +14,9 @@ if [ -f "$PID_FILE" ] ; then
     exit 1
 fi
 # set JVM arguments
-pwd
-JVM_ARGS="-Dlogback.configurationFile=$APP_DIR/config/logger.xml -cp $APP_DIR/lib/*:wallcology-notifier.jar"
+JVM_ARGS="-Dlogback.configurationFile=$APP_DIR/config/logger.xml -cp $APP_DIR/lib/*:$BIN_DIR/wallcology-notifier.jar"
 # startup
-nohup "$JAVA" $JVM_ARGS -jar "$BIN_DIR"/"$@" > "$OUT_FILE" 2>&1 &
+"$JAVA" $JVM_ARGS "ltg.es.wallcology.notifier.Notifier" "$APP_DIR/config/notifier.xml" &
 PID="$!"
 echo "$PID" > "$PID_FILE"
-echo "Started, pid: $PID"
+echo "Started wallcology-notifier-agent, pid: $PID"
